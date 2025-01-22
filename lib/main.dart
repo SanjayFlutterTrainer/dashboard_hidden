a7557eb09f7034e473254934bcdb8af4b77da47b
SanjayFlutterTrainer <fluttertrainerfuturakochi@gmail.com>
2025-01-22 09:46:37 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:etra/providers/userProvider.dart';
 import 'package:etra/views/users.dart';
-//hello
+//hello updated
 void main() {
   runApp(const MyApp());
 }
